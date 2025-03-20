7902929bdc20e6ff6e40e3b64fb45278c12aa267
albanogabriel <albanogabriel33@gmail.com>
2025-03-20 10:05:12 -0300

    feat: get open connection with database

---
+++ src/lib/prisma.ts
@@ -0,0 +1,22 @@
+// É usado para sempre manter a conexão com o banco de dados, se não é possível que sempre estoure o banco
+
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
+
+// vou usar para chamar meu banco de dados
+export const db = prisma;
